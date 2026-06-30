2d6935d3735081b0f6013c2ff5f2bbf9c6dd92a4
HermeSoftware <yagizberkworks@gmail.com>
2026-07-01 00:52:45 +0300

    build hatalarini yoksay

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    // Build sırasında TypeScript hatalarını yoksay
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
